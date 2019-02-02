911644148c2a270197444877c01f11c4ad56cb45
doohong <wnghd94@gmail.com>
2019-02-02 21:16:12 +0900

    fail on unknown properteis

--- src/test/java/com/doohong/restapi/event/EventControllerTests.java
+++ src/test/java/com/doohong/restapi/event/EventControllerTests.java
@@ -3,12 +3,9 @@ package com.doohong.restapi.event;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.hateoas.MediaTypes;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
@@ -20,7 +17,6 @@ import java.time.LocalDateTime;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -32,7 +28,7 @@ public class EventControllerTests {
     ObjectMapper objectMapper;
 
     @Test
-    public void createEvent() throws Exception {
+    public void createEvent_Bed_request() throws Exception { //필요 이상 입력값 입력시 400오류
         Event event = Event.builder()
                 .id(100)
                 .name("Spring")
@@ -45,7 +41,33 @@ public class EventControllerTests {
                 .maxPrice(200)
                 .limitOfEnrollment(100)
                 .location("강남역 D2 스타텁 팩토리")
+                .free(true)
+                .offline(false)
                 .eventStatus(EventStatus.PUBLISHED)
+                .build();
+
+        mockMvc.perform(post("/api/events")
+                .contentType(MediaType.APPLICATION_JSON_UTF8)
+                .accept(MediaTypes.HAL_JSON)
+                .content(objectMapper.writeValueAsString(event)))
+                .andDo(print())
+                .andExpect(status().isBadRequest())
+                ;
+    }
+
+    @Test
+    public void createEvent() throws Exception { //필요이상 입력값 필요값 외의 값  무시
+        EventDto event = EventDto.builder()
+                .name("Spring")
+                .description("REST API Development with Spring")
+                .beginEnrollmentDateTime(LocalDateTime.of(2018,11,23,14,22))
+                .closeEnrollmentDateTime(LocalDateTime.of(2018,11,24,14,22))
+                .beginEventDateTime(LocalDateTime.of(2018,11,25,14,22))
+                .endEventDateTime(LocalDateTime.of(2018,11,26,14,22))
+                .basePrice(100)
+                .maxPrice(200)
+                .limitOfEnrollment(100)
+                .location("강남역 D2 스타텁 팩토리")
                 .build();
 
         mockMvc.perform(post("/api/events")
