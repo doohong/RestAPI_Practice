7c96360959cca2622a20d7c8136d53bab6b2a06c
doohong <wnghd94@gmail.com>
2019-02-03 18:03:19 +0900

    validate wrong data

--- src/main/java/com/doohong/restapi/event/EventController.java
+++ src/main/java/com/doohong/restapi/event/EventController.java
@@ -1,10 +1,10 @@
 package com.doohong.restapi.event;
 
 import org.modelmapper.ModelMapper;
-import org.modelmapper.internal.Errors;
 import org.springframework.hateoas.MediaTypes;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,20 +13,25 @@ import javax.validation.Valid;
 import java.net.URI;
 
 import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
-import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
 
 @Controller
 @RequestMapping(value = "/api/events",produces = MediaTypes.HAL_JSON_UTF8_VALUE)
 public class EventController {
     private final  EventRepository eventRepository;
     private final ModelMapper modelMapper;
-    public EventController(EventRepository eventRepository, ModelMapper modelMapper){
+    private final EventValidator eventValidator;
+    public EventController(EventRepository eventRepository, ModelMapper modelMapper, EventValidator eventValidator){
         this.eventRepository = eventRepository;
         this.modelMapper = modelMapper;
+        this.eventValidator = eventValidator;
     }
 
     @PostMapping
     public ResponseEntity createEvent(@RequestBody @Valid EventDto eventDto, Errors errors){
+        if(errors.hasErrors()){
+            return ResponseEntity.badRequest().build();
+        }
+        eventValidator.validate(eventDto,errors);
         if(errors.hasErrors()){
             return ResponseEntity.badRequest().build();
         }
--- src/test/java/com/doohong/restapi/event/EventControllerTests.java
+++ src/test/java/com/doohong/restapi/event/EventControllerTests.java
@@ -89,5 +89,23 @@ public class EventControllerTests {
                         .content(this.objectMapper.writeValueAsString(eventDto)))
                     .andExpect(status().isBadRequest());
     }
+    @Test
+    public void createEvent_Bad_request_Wrong_Input() throws Exception { //필요 이상 입력값 입력시 400오류
+        EventDto eventDto = EventDto.builder().name("Spring")
+                .description("REST API Development with Spring")
+                .beginEnrollmentDateTime(LocalDateTime.of(2018,11,26,14,22))
+                .closeEnrollmentDateTime(LocalDateTime.of(2018,11,25,14,22))
+                .beginEventDateTime(LocalDateTime.of(2018,11,24,14,22))
+                .endEventDateTime(LocalDateTime.of(2018,11,23,14,22))
+                .basePrice(10000)
+                .maxPrice(200)
+                .limitOfEnrollment(100)
+                .location("강남역 D2 스타텁 팩토리")
+                .build();
+        this.mockMvc.perform(post("/api/events")
+                .contentType(MediaType.APPLICATION_JSON_UTF8)
+                .content(this.objectMapper.writeValueAsString(eventDto)))
+                .andExpect(status().isBadRequest());
+    }
 
 }
